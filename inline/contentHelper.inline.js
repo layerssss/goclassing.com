bf7f0f81c6d0e34d8e9ffaa12eeeb4e1cbd2cd09
Michael Yin <layerssss@gmail.com>
2012-04-28 19:42:38 +0800

    utilize markdown as our new format

--- inline/contentHelper.inline.js
+++ inline/contentHelper.inline.js
@@ -1,14 +1,17 @@
 /*<!--*//*-->##Chapter1 : chapter title
 ###Today's coursework : smaller title
 ######here they are : smallest title
-============== : horizontal line
+horizontal line:
+*********
 [my blog](http://my.blog.net/) : link
 ![It's me](http://my.blog.net/avatar,gif) : image
 _This will also be italic_ : italisized text
 __This will also be bold__ : bold text
 list:
+
 * Item 1
 * Item 2
+
 As Kanye West said(Blockquotes):
 
 > We're living the future so
