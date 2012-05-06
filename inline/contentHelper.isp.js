46c91f4ec74c085e4b8e577cb8201e2c49b40ec6
Michael Yin <layerssss@gmail.com>
2012-05-06 16:47:10 +0800

    Update inline/contentHelper.isp.js

--- inline/contentHelper.isp.js
+++ inline/contentHelper.isp.js
@@ -1,19 +1,21 @@
 /*<!--*//*-->##Chapter1 : chapter title
 ###Today's coursework : smaller title
 ######here they are : smallest title
-horizontal line:
 *********
+:horizontal line
 [my blog](http://my.blog.net/) : link
-![It's me](http://my.blog.net/avatar,gif) : image
+![It's me](http://my.blog.net/avatar.gif) : image
 _This will also be italic_ : italisized text
 __This will also be bold__ : bold text
-list:
+list:(don't forget empty lines before and after the items)
 
 * Item 1
-* Item 2
+* Item 2(don't forget a space after the asterisk)
 
 As Kanye West said(Blockquotes):
 
 > We're living the future so
-> the present is our past.<!--*/
+> the present is our past.
+
+(don't forget empty lines before and after the quotes)<!--*/
 //-->
