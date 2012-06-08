88ae359db17a810ca8d7676e0c42f73b99b75225
Michael Yin <layerssss@gmail.com>
2012-06-08 08:09:42 +0800

    add 'viewport' metadata to fix layout on small device

--- master/cframe.master.js
+++ master/cframe.master.js
@@ -4,6 +4,7 @@ var c = arguments[0].course;
 <html lang="en">
 <head>
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="" />
     <meta name="author" content="" />
     <!-- Le styles -->
